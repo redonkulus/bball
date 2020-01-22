99694851e1c7cbd6277fea4bdf6d8e025293d0c0
redonkulus <seth@bertalotto.net>
2020-01-21 22:00:13 -0800

    feat: add/remove player support

--- bball.js
+++ bball.js
@@ -1,25 +1,29 @@
 (function() {
-    const defaultPlayers = [
-        'Evan (F)',
-        'Joel (G)',
-        'Ian (C)',
-        'Jacob (F)',
-        'Sachitt (G)',
-        'Patrick (C)',
-        'Yuva (F)',
-        'Jared (G)',
-        'Ethan (F)',
-        'Ben (G)'
-    ];
+    const addPlayerNode = document.getElementById('add-player');
     const playersNode = document.getElementById('players');
     const lineupNode = document.getElementById('lineup');
     const shuffleNode = document.getElementById('shuffle');
+
+    let playerList = JSON.parse(localStorage.getItem('playerList') || '[]'); // meta data for all players
     let dragSrcEl = null; // used to store dragged item
+    let dragCurIndex = 0; // used to track currently dragged item index in player list
     let pageY = 0; // used to determine drag direction
 
-    function shuffle(array) {
-        const list = [...array];
-        let currentIndex = list.length;
+    function move(index, delta) {
+        const newIndex = index + delta;
+        // Already at the top or bottom.
+        if (newIndex < 0  || newIndex === playerList.length) {
+            return;
+        }
+        // Sort the indixes
+        const indexes = [index, newIndex].sort((a, b) => a - b);
+
+        // Replace from lowest index, two elements, reverting the order
+        playerList.splice(indexes[0], 2, playerList[indexes[1]], playerList[indexes[0]]);
+    };
+
+    function shufflePlayers() {
+        let currentIndex = playerList.length;
         let temporaryValue;
         let randomIndex;
 
@@ -30,34 +34,40 @@
             currentIndex -= 1;
 
             // And swap it with the current element.
-            temporaryValue = list[currentIndex];
-            list[currentIndex] = list[randomIndex];
-            list[randomIndex] = temporaryValue;
+            temporaryValue = playerList[currentIndex];
+            playerList[currentIndex] = playerList[randomIndex];
+            playerList[randomIndex] = temporaryValue;
         }
 
-        return list;
+        renderPlayers();
     }
 
-    function shufflePlayers() {
-        const activePlayers = getActivePlayers();
-        const players = shuffle(activePlayers);
-        renderPlayers(players);
-        renderLineup();
-    }
-
-    function getActivePlayers() {
-        const activeList = Array.from(document.getElementsByClassName('active'));
-        const players = [];
-
-        if (activeList) {
-            activeList.forEach(player => {
-                if (player.checked) {
-                    players.push(player.value);
-                }
-            });
+    function addPlayer(e) {
+        // prevent form from submitting
+        if (e.preventDefault) {
+            e.preventDefault();
         }
 
-        return players;
+        let formData = new FormData(addPlayerNode);
+        const name = formData.get('add-player-input');
+
+        playerList.push({ name, active: true });
+
+        renderPlayers();
+    }
+
+    function removePlayer(id) {
+        for (let x = 0; x < playerList.length; x++) {
+            if (playerList[x].name === id) {
+                playerList.splice(x, 1);
+                renderPlayers();
+                break;
+            }
+        }
+    }
+
+    function savePlayers() {
+        localStorage.setItem('playerList', JSON.stringify(playerList));
     }
 
     /**
@@ -70,6 +80,7 @@
         dragSrcEl = e.target;
         e.dataTransfer.effectAllowed = 'move';
         pageY = e.pageY;
+        dragCurIndex = parseInt(e.target.dataset.idx, 10);
     }
 
     function handleDragOver(e) {
@@ -101,12 +112,13 @@
             if (e.pageY > pageY) {
                 // player was moved down
                 e.target.parentNode.insertBefore(dragSrcEl, e.target.nextElementSibling);
+                move(dragCurIndex, 1);
             } else {
                 // player was moved up
                 e.target.parentNode.insertBefore(dragSrcEl, e.target);
+                move(dragCurIndex, -1);
             }
-            // re-render lineup based on new order
-            renderLineup();
+            renderPlayers();
         }
 
         return false;
@@ -121,58 +133,83 @@
     /**
      * Renderers
      */
-    function renderPlayers(players = defaultPlayers) {
+    function renderPlayers() {
         const markup = [];
-        players.forEach(p => {
+        playerList.forEach((p, idx) => {
+            const checked = p.active === true ? 'checked' : '';
             markup.push(`
-                <li class="list-group-item draggable" draggable="true">
-                    <input class="active" type="checkbox" value="${p}" checked> ${p}</input>
+                <li id="${p.name}" data-idx=${idx} class="list-group-item d-flex justify-content-between draggable" draggable="true">
+                    <label class="mb-0 align-middle">
+                        <input class="active" type="checkbox" value="${p.name}" ${checked}> ${p.name}</input>
+                    </label>
+                    <button class="remove btn btn-sm">
+                        <svg viewBox="0 0 96 96" width="24" height="24" style="enable-background:new 0 0 96 96;">
+                            <path d="M40.5,66.8V39.3c0-0.4-0.1-0.7-0.4-0.9S39.6,38,39.3,38h-2.5c-0.4,0-0.7,0.1-0.9,0.4 s-0.4,0.5-0.4,0.9v27.5c0,0.4,0.1,0.7,0.4,0.9s0.5,0.4,0.9,0.4h2.5c0.4,0,0.7-0.1,0.9-0.4S40.5,67.1,40.5,66.8z M50.5,66.8V39.3 c0-0.4-0.1-0.7-0.4-0.9S49.6,38,49.3,38h-2.5c-0.4,0-0.7,0.1-0.9,0.4s-0.4,0.5-0.4,0.9v27.5c0,0.4,0.1,0.7,0.4,0.9s0.5,0.4,0.9,0.4 h2.5c0.4,0,0.7-0.1,0.9-0.4S50.5,67.1,50.5,66.8z M60.5,66.8V39.3c0-0.4-0.1-0.7-0.4-0.9S59.6,38,59.3,38h-2.5 c-0.4,0-0.7,0.1-0.9,0.4s-0.4,0.5-0.4,0.9v27.5c0,0.4,0.1,0.7,0.4,0.9s0.5,0.4,0.9,0.4h2.5c0.4,0,0.7-0.1,0.9-0.4 S60.5,67.1,60.5,66.8z M39.3,28h17.5l-1.9-4.6c-0.2-0.2-0.4-0.4-0.7-0.4H41.8c-0.3,0.1-0.5,0.2-0.7,0.4L39.3,28z M75.5,29.3v2.5 c0,0.4-0.1,0.7-0.4,0.9S74.6,33,74.3,33h-3.8v37c0,2.2-0.6,4-1.8,5.6S66,78,64.3,78H31.8c-1.7,0-3.2-0.8-4.4-2.3s-1.8-3.4-1.8-5.5 V33h-3.8c-0.4,0-0.7-0.1-0.9-0.4s-0.4-0.5-0.4-0.9v-2.5c0-0.4,0.1-0.7,0.4-0.9s0.5-0.4,0.9-0.4h12.1l2.7-6.5c0.4-1,1.1-1.8,2.1-2.5 s2-1,3.1-1h12.5c1,0,2.1,0.3,3.1,1s1.7,1.5,2.1,2.5l2.7,6.5h12.1c0.4,0,0.7,0.1,0.9,0.4S75.5,28.9,75.5,29.3z"/>
+                        </svg>
+                    </button>
                 </li>
             `);
         });
         playersNode.innerHTML = markup.join('');
+        renderLineup();
+        savePlayers();
     }
 
     function renderLineup() {
-        const perQuarter = 5;
-        const periods = 8;
-        const total = perQuarter * periods;
-
-        let x = 1;
-        let start = 0;
-        let period = 1;
-        let currentPlayers = [];
-
         const listMarkup = [];
-        const players = getActivePlayers();
+        if (!playerList.length) {
+            listMarkup.push('Please add players to generate a lineup.');
+        } else {
+            const perQuarter = 5;
+            const periods = 8;
+            const total = perQuarter * periods;
+    
+            let x = 1;
+            let start = 0;
+            let period = 1;
 
-        while (x <= total) {
-            // add player
-            currentPlayers.push(players[start]);
+            let currentPlayers = [];
+            const activePlayers = playerList.filter(p => p.active === true);
 
-            // determine players whenever perQuarter limit is met
-            if (x > 0 && x % perQuarter === 0) {
-                // print players
-                listMarkup.push(`<li class="list-group-item">${period}. ${currentPlayers.join(', ')}</li>`);
+            // loop through each player to add to lineup
+            while (x <= total) {
+                if (activePlayers.length) {
+                    const { name } = activePlayers[start];
 
-                // increment to display next period
-                period++;
-
-                // reset current players
-                currentPlayers = [];
+                    // add player if not already added
+                    if (!currentPlayers.includes(name)) {
+                        currentPlayers.push(name);
+                    }
+        
+                    // determine players whenever perQuarter limit is met
+                    if (x > 0 && x % perQuarter === 0) {
+                        // print players
+                        listMarkup.push(`<li class="list-group-item">${period}. ${currentPlayers.join(', ')}</li>`);
+        
+                        // increment to display next period
+                        period++;
+        
+                        // reset current players
+                        currentPlayers = [];
+                    }
+                }
+    
+                start++;
+    
+                if (start > (activePlayers.length - 1)) {
+                    start = 0;
+                }
+    
+                x++;
             }
-
-            start++;
-
-            if (start > (players.length - 1)) {
-                start = 0;
-            }
-
-            x++;
         }
 
         // output markup
-        lineupNode.innerHTML = listMarkup.join('');
+        if (listMarkup.length) {
+            lineupNode.innerHTML = listMarkup.join('');
+        } else {
+            lineupNode.innerHTML = 'No active players to lineup.';
+        }
     }
 
     /**
@@ -180,15 +217,21 @@
      */
     function attachListeners() {
         playersNode.addEventListener('click', event => {
-            if (event.target.tagName !== 'input') {
-                const input = event.target.firstElementChild;
-                if (input) {
-                    input.checked = !input.checked;
-                    renderLineup();
-                }
+            const { id } = event.target.closest('li');
+
+            // handle remove player
+            if (event.target.closest('button')) {
+                removePlayer(id);
+            } else {
+                for (let x = 0; x < playerList.length; x++) {
+                    if (playerList[x].name === id) {
+                        playerList[x].active = !playerList[x].active;
+                        renderPlayers();
+                        break;
+                    }
+                };
             }
         });
-        playersNode.addEventListener('change', renderLineup);
 
         // drag and drop listeners
         playersNode.addEventListener('dragstart', handleDragStart, false);
@@ -201,10 +244,17 @@
         // shuffle player button
         shuffleNode.addEventListener('click', shufflePlayers);
         shuffleNode.addEventListener('tap', shufflePlayers);
+
+        // add player form
+        addPlayerNode.addEventListener('submit', addPlayer);
+        addPlayerNode.addEventListener('click', e => {
+            if (e.target.tagName.toLowerCase() === 'input') {
+                e.target.select();
+            }
+        });
     }
 
     // init
     renderPlayers();
-    renderLineup();
     attachListeners();
 })();
